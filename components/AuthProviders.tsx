a98dce5adb17f94966b23fd123af0a2be9dedbd5
xtadeushx <alekscherkasov13@gmail.com>
2023-10-01 14:59:26 +0300

    feat: add auth

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+"use client"
+import { useEffect, useState } from "react";
+import { getProviders, signIn } from "next-auth/react";
+
+interface Provider {
+  id: string;
+  name: string;
+  type: string;
+  signUrl?: string;
+  callbackUrl: string;
+  signUrlParams?: Record<string, string> | null;
+}
+type Providers = Record<string, Provider>
 
 const AuthProviders = () => {
-  return (
-    <div>AuthProviders</div>
-  )
+  const [providers, setProviders] = useState<Providers | null>(null);
+
+  useEffect(() => {
+    const fetchProviders = async () => {
+      const res = await getProviders();
+      setProviders(res);
+    };
+    fetchProviders();
+  }, [])
+
+
+  if (providers) {
+    return (
+      <div className="">
+        {Object.values(providers).map((provider: Provider, index) => (
+          <button key={index}>{provider.id}</button>
+        ))}
+      </div>
+    )
+  }
 }
 
 export default AuthProviders
